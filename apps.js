0bbf45e92f2fd3f3cd3f54fab15e3ad6cb79aa49
ANILA <160001699+ANILAALLAHRAKHA@users.noreply.github.com>
2024-03-07 19:53:33 +0500

    not work

---
+++ apps.js
@@ -0,0 +1,14 @@
+var num1 = 4*1
+var num2 = 4*2
+var num3 = 4*3
+var num4 = 4*4
+var num5 = 4*5
+var num6 = 4*6
+var num7 = 4*7
+var num8 = 4*8
+var num9 = 4*9
+var num10 = 4*10
+document.write("4 x 1=" , num1 , "<br />", "4x2=" , num2  , "<br/ >" , "4*3=" , num3 ,"<br/ >" ,   "4*4=" , num4 ,
+"<br />" ,"4x5=" ,num5 , "<br />" ,  "4x6=" ,num6 , "<br />"
+  
+);
